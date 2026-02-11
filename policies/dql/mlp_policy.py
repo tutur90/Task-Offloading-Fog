c446ae9dbe35dc722fcdb94f121c0cc5654a8e1c
tutu90 <arthur.garon@gmail.com>
2026-02-12 00:43:01 +0100

    s

--- policies/dql/mlp_policy.py
+++ policies/dql/mlp_policy.py
@@ -31,7 +31,7 @@ class MLP(nn.Module):
 
 class MLPPolicy(DQNPolicy):
     def __init__(self, env, config):
-        super().__init__(env, config, device="cpu")
+        super().__init__(env, config)
         
     def _init_model(self, env, config):
         self.model = MLP(d_in=self.d_obs, d_pos=self.n_observations, d_task=4, output_size=self.num_actions, **config["model"])
