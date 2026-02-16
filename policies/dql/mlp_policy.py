4fe5e1dd08f27f988b09cbfc24b9c17c1c7ad2e7
tutu90 <arthur.garon@gmail.com>
2026-02-16 17:15:17 +0100

    s

--- policies/dql/mlp_policy.py
+++ policies/dql/mlp_policy.py
@@ -10,7 +10,7 @@ class MLPPolicy(DQNPolicy):
 
     def _init_model(self, env, config, dataset=None):
         self.model = MLP(d_in=self.d_obs, d_pos=self.n_observations, d_task=4, output_size=self.num_actions, **config["model"])
-        self.model.register_norm(self._make_observation(env, None, self.obs_type)[0], dataset=dataset)
+        self.model.register_norm(self._make_observation(env, None, self.obs_type)[0], dataset=dataset, device=self.device)
         self.target_model = copy.deepcopy(self.model)
         self.target_model.eval()
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
