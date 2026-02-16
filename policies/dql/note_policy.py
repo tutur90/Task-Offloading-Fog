5400f99277030ab8df5bcb3dee6b74f8819c8d57
tutu90 <arthur.garon@gmail.com>
2026-02-16 17:19:45 +0100

    s

--- policies/dql/note_policy.py
+++ policies/dql/note_policy.py
@@ -12,7 +12,7 @@ class NOTEPolicy(DQNPolicy):
         
     def _init_model(self, env, config, dataset=None):
         self.model = NOTE(d_in=self.d_obs, d_pos=self.n_observations, d_task=4, output_size=self.num_actions, **config["model"]).to(self.device)
-        self.model.register_norm(self._make_observation(env, None, self.obs_type)[0], dataset=dataset)
+        self.model.register_norm(self._make_observation(env, None, self.obs_type)[0], dataset=dataset, device=self.device)
         self.target_model = copy.deepcopy(self.model)
         self.target_model.eval()
         self.optimizer = optim.AdamW(self.model.parameters(), lr=self.lr, weight_decay=config["training"].get("weight_decay", 0.01))
